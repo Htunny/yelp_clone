7b7310a26f6784a89ebc2c363598c49037c4ae61
Andrew <andy_htun@hotmail.com>
2016-01-18 16:37:54 +0000

    minimum 3 characters to add new restaurant

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -30,6 +30,17 @@ feature 'restaurants' do
       expect(page).to have_content 'Hawksmoor'
       expect(current_path).to eq '/restaurants'
     end
+
+    context 'an invalid restaurant' do
+      it 'does not let you submit a name that is too short' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'Ha'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'Ha'
+        expect(page).to have_content 'error'
+      end
+    end
   end
 
   context 'viewing restaurants' do
@@ -67,4 +78,5 @@ feature 'restaurants' do
       expect(page).to have_content 'Restaurant deleted successfully'
     end
   end
+
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 require 'spec_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
-
-
-describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews }
+
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "Ha")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
 end
