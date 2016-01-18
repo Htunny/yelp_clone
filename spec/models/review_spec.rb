d10c5c2e0bc1216adfdc8be1708c45f7abc349e8
Andrew <andy_htun@hotmail.com>
2016-01-18 16:44:26 +0000

    restaurant uniqueness function added

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   it { is_expected.to belong_to(:restaurant).dependent(:destroy) }
+
+  it 'is invalid if rating is more than 5' do
+    review = Review.new(rating: 10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
